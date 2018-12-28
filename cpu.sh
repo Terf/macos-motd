cf44b37c1c26be27550c819f2621fe033bf49b80
Tim Robert-Fitzgerald <tim.terf@gmail.com>
2018-12-28 15:49:00 -0600

    better perf for cpu.sh, changed colors
    also % out of 400 bc 4 cores

--- cpu.sh
+++ cpu.sh
@@ -1,22 +1,16 @@
 #!/bin/bash
 
 usage=`ps -A -o %cpu | awk '{s+=$1} END {print s}'`
-pct=`bc -l <<< "${usage}/200"`
-echo "CPU usage: ${usage}%"
-
-bar="====================================================================="
-buffer=""
-red='\033[0;31m'
+pct=`bc -l <<< "${usage}/400"` # 400 because 4 cores on my machine
+printf "CPU usage: "
+printf %.2f `echo "${pct}*100" | bc -l`
+echo "%"
+bar="==============================================="
+# red='\033[0;31m'
+red='\e[33m'
 nc='\033[0m'
 count=${#bar}
-buffer+="  ["
-for (( i=0; i<$count; i++ )); do
-	pctnow=`bc -l <<< "${i}/${count}"`
-	colored=`bc -l <<< "${pctnow} > ${pct}"`
-	if [[ $colored == '0' ]]; then
-		buffer+="${red}${bar:$i:1}"
-	else
-		buffer+="${nc}${bar:$i:1}"
-	fi
-done
-printf "${buffer}]\n"
+index=`bc -l <<< "${pct} * ${count}"`
+index=`printf "%.0f" "${index}"`
+printf "  [${red}${bar:0:$index}${nc}${bar:$index:$count}]\n"
+
